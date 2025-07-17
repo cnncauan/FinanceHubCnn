b1c4f5162412567526e53825356eb481b2bce31a
Marcos Cauan <marcos.cauan@somapay.com.br>
2025-07-17 00:55:24 -0300

    refactored to avoid repettion

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,4 +1,18 @@
 import styles from './Category.module.css'
+import videos from "../../json/videos.json"
+
+export const categories = [
+  "Bitcoin",
+  "Walllet",
+  "Memecoin",
+  "Trade",
+  "Rwa",
+  "Rendafixa",
+]
+
+export function filterCategory(id) {
+  return videos.filter( video => video.category === categories[id] )
+}
 
 function Category({ category, children}) {
     return(
