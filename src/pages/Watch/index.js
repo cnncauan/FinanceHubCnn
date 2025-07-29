cf4a227825dded34bc77f3e16ec164f691eac1be
Marcos Cauan <marcos.cauan@somapay.com.br>
2025-07-28 23:03:39 -0300

    creted dynamic route for page Watch

--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -2,8 +2,18 @@ import styles from './Watch.module.css'
 import Header from '../../components/Header'
 import Container from '../../components/Container'
 import Footer from '../../components/Footer'
+import { useParams } from 'react-router-dom'
+import videos from '../../json/videos.json'
+import PageNotFOund from '../PageNotFound'
 
 function Watch(){
+    const params = useParams()
+    const video = videos.find((video) => {return video.id === params.id})
+    
+    if(!video) {
+        return <PageNotFOund/>
+    }
+
     return (
         <>
             <Header/>
@@ -11,10 +21,13 @@ function Watch(){
                 <section className={styles.watch}>
                     <iframe 
                         width="854" height="480" 
-                        src="https://www.youtube.com/embed/lZn2CHO-1ic?si=VndPI1fgC1mJMs1l" 
-                        title="YouTube video player" frameborder="0" allow="accelerometer; 
+                        src={`https://www.youtube.com/embed/${video.id}`} 
+                        title="YouTube video player" 
+                        frameborder="0" 
+                        allow="accelerometer; 
                         autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; 
-                        web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>    
+                        web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>    
                     </iframe>
                 </section>
             </Container>
