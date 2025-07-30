3df6fbcfc8d95965e761330e26f640c1468e8f01
Marcos Cauan <marcos.cauan@somapay.com.br>
2025-07-29 23:55:01 -0300

    adjusted file for receive new className

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,16 +2,22 @@ import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 import imgLogo from "./moneyy.png"
 
+import HomeIcon from '@mui/icons-material/Home';
+import MovieIcon from '@mui/icons-material/Movie';
+import SearchIcon from '@mui/icons-material/Search';
+
+
 function Header() {
     return(
         <header className={styles.header}>
-            <Link to="/">
+            <Link to="/" className={styles.logoLink}>
                 <img src={imgLogo} alt="Logo FinanceHubCnn" className={styles.logo} />
                 <span>FinanceHubCnn</span>
             </Link>
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/Watch">Assitir</Link>
+                <Link to="/" className={styles.navLink}>Home<HomeIcon sx={{fontSize:20}} /></Link>
+                <Link to="/watch" className={styles.navLink}>Assitir<MovieIcon sx={{fontSize:20}} /></Link>
+                <Link to="/search" className={styles.navLink}>Pesquisar<SearchIcon sx={{ fontSize: 20 }} /></Link>
             </nav>
         </header>
     );
