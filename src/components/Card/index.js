e201ff78e7264b741bf2cbcc001b1a61f347b3d6
Marcos Cauan <marcos.cauan@somapay.com.br>
2025-08-07 22:55:57 -0300

    add icon Fovorite

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import styles from './Card.module.css'
 
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
 function Card({ id }){
     return(
         <section className={styles.card}>
@@ -9,8 +11,10 @@ function Card({ id }){
                     src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} 
                     alt='Capa'
                 />
-                
             </Link>
+            <figure className={styles.icon}>
+                <FavoriteIcon className={styles.capa}/>
+            </figure>
         </section>
     );
 }
