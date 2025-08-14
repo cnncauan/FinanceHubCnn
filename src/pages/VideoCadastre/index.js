79103b17e79099fb3a7a427300e43db86bb1fda6
Marcos Cauan <marcos.cauan@somapay.com.br>
2025-08-13 22:15:46 -0300

    created page for register video

---
+++ src/pages/VideoCadastre/index.js
@@ -0,0 +1,20 @@
+import Footer from "../../components/Footer"
+import Form from "../../components/Form"
+
+import Header from "../../components/Header"
+import Container from "../../components/Container"
+
+
+function VideoCadastre () {
+    return (
+        <>
+            <Header/>
+            <Container>
+                <Form/>
+            </Container>
+            <Footer/>
+        </>
+    )
+}
+
+export default VideoCadastre
